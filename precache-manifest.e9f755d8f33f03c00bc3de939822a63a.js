be79111e5a3d606df9f6401dbdbb7f15ccaa9570
karthikeyanvk-techie <karthikeyanvk.techie@gmail.com>
2020-07-05 16:37:32 +0530

    Updates

---
+++ precache-manifest.e9f755d8f33f03c00bc3de939822a63a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "926f9a0bc65ff97f9c80f984dda85c2a",
+    "url": "/karthikeyanvk-techie/reluapad/index.html"
+  },
+  {
+    "revision": "d7f7305ca4d62354bb8d",
+    "url": "/karthikeyanvk-techie/reluapad/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fbc97e1da162f78f4916",
+    "url": "/karthikeyanvk-techie/reluapad/static/js/2.c23002e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/karthikeyanvk-techie/reluapad/static/js/2.c23002e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f7305ca4d62354bb8d",
+    "url": "/karthikeyanvk-techie/reluapad/static/js/main.aa43dd5a.chunk.js"
+  },
+  {
+    "revision": "78ebdc6efd80006eca4f",
+    "url": "/karthikeyanvk-techie/reluapad/static/js/runtime-main.7b60808a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/karthikeyanvk-techie/reluapad/static/media/logo.5d5d9eef.svg"
+  }
+]);
