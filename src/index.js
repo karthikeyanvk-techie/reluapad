01cd5b696374630728c8bf4c61f9ff42a5a41ac9
karthikeyanvk-techie <karthikeyanvk.techie@gmail.com>
2020-07-05 22:46:49 +0530

    test commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes />
     </Router>
   </React.StrictMode>,
