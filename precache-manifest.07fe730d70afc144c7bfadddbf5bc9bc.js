d7efd0d0804f3e8f06eb24fa70b11a06cdc4d87c
karthikeyanvk-techie <karthikeyanvk.techie@gmail.com>
2020-07-08 20:00:12 +0530

    Updates

---
+++ precache-manifest.07fe730d70afc144c7bfadddbf5bc9bc.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0147be7f14d317066582998e8ae91aef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19b1d4c2c3b1cd13467a",
+    "url": "/static/css/main.ca1e38a6.chunk.css"
+  },
+  {
+    "revision": "8aa70d879b8d4c70b26a",
+    "url": "/static/js/2.18a7dbd5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.18a7dbd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b1d4c2c3b1cd13467a",
+    "url": "/static/js/main.0d78b7b2.chunk.js"
+  },
+  {
+    "revision": "5766f7b16f0637341c43",
+    "url": "/static/js/runtime-main.48d0a7eb.js"
+  },
+  {
+    "revision": "00ce23caac2c875bcc6db8e4afe5c532",
+    "url": "/static/media/Simple-Line-Icons.00ce23ca.svg"
+  },
+  {
+    "revision": "b2892aa62b0fb2c21c8d7700e2ef6e56",
+    "url": "/static/media/Simple-Line-Icons.b2892aa6.ttf"
+  },
+  {
+    "revision": "dacd16c9a3d2cb61b472a35979015bf3",
+    "url": "/static/media/Simple-Line-Icons.dacd16c9.eot"
+  },
+  {
+    "revision": "4e27be52305496ebcb364f6373bb187a",
+    "url": "/static/media/app.4e27be52.png"
+  },
+  {
+    "revision": "f2b3f4989da0295a166cdb733237cb36",
+    "url": "/static/media/arrow-object-dark.f2b3f498.svg"
+  },
+  {
+    "revision": "35218858a32caf218a9166ed4b5e2580",
+    "url": "/static/media/cc-bg-1.35218858.jpg"
+  },
+  {
+    "revision": "e165a32b4e032329a38dff51bda427df",
+    "url": "/static/media/header-bg.e165a32b.jpg"
+  },
+  {
+    "revision": "779e04a8ce4176fb537b1760ab58a191",
+    "url": "/static/media/img-02.779e04a8.jpg"
+  },
+  {
+    "revision": "0fd88d706010e0979efd38ebe1075856",
+    "url": "/static/media/img-05.0fd88d70.jpg"
+  },
+  {
+    "revision": "5c007eac3865a3f0d24a72a1ea014818",
+    "url": "/static/media/logo-black.5c007eac.png"
+  },
+  {
+    "revision": "6dd852ff9e379cf915e47eaeda88294f",
+    "url": "/static/media/logo.6dd852ff.png"
+  },
+  {
+    "revision": "1dab0eed2834b4940d588c2d96d382e5",
+    "url": "/static/media/mfglabsiconset-webfont.1dab0eed.svg"
+  },
+  {
+    "revision": "b0a0976ce87a47700c6e689226e7a411",
+    "url": "/static/media/mfglabsiconset-webfont.b0a0976c.eot"
+  },
+  {
+    "revision": "ef329adb46ed36028ac928befa3a17c8",
+    "url": "/static/media/mfglabsiconset-webfont.ef329adb.woff"
+  },
+  {
+    "revision": "f130ab32e5da85a090f22822ddec64e6",
+    "url": "/static/media/mfglabsiconset-webfont.f130ab32.ttf"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "bd6954378214ff2e1664307c7e5853e2",
+    "url": "/static/media/responsive-01.bd695437.png"
+  },
+  {
+    "revision": "3292b0829b88abd471ece87d67171dee",
+    "url": "/static/media/responsive-02.3292b082.png"
+  },
+  {
+    "revision": "0264b8fece3b536ebb8b8827ecff9b04",
+    "url": "/static/media/responsive-03.0264b8fe.png"
+  },
+  {
+    "revision": "1f2a36f9dea7f15f0bd548c7a331fcb0",
+    "url": "/static/media/responsive-04.1f2a36f9.png"
+  },
+  {
+    "revision": "83cab3c3d82f9e6e786a00bd268bab78",
+    "url": "/static/media/start-your-business.83cab3c3.svg"
+  },
+  {
+    "revision": "8221bc81454d40fd7900bda6d88770fd",
+    "url": "/static/media/staticmap.8221bc81.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reluapad/precache-manifest.4f0269ba7f30c89ccac1a907142c3633.js"
+  "/precache-manifest.07fe730d70afc144c7bfadddbf5bc9bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reluapad/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
